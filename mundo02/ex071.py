1fb4cc20106601dcb6f81e37ed74f351a800c5b4
Kayky Queiroz <kaykysousa617@gmail.com>
2024-10-03 15:20:06 -0300

    Deleting Unused Repositories

--- mundo02/ex071.py
+++
@@ -1,8 +0,0 @@
-""" 
-Crie um programa que simule um CAIXA ELETRONICO.
-no inicio, pergunte ao usuario qual o valor que vai ser sacado (numero inteiro).
-e o programa vai informar quantas cedulas de cada valor serão entregues 
-
-OBS: considere que o caixa possui cédulas de R$50, R$20, $10 e R$1. 
-
-"""
